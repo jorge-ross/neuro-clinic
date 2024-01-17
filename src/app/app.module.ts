7433feaf0707deb41fcc1fd60e042fa90f88c285
Jorge Ross <jorg.ross.13@gmail.com>
2024-01-16 18:55:52 -0600

    fix: files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 
-
-// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InsightComponent } from './components/insight/insight.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,13 +9,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { BlogHomeComponent } from './components/blog-home/blog-home.component';
-import { ServicesComponent } from './psic-services/services.component';
+import { ServicesComponent } from './components/psic-services/services.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
-  { path: 'Contact', component: ContactFormComponent },
-  { path: 'servicios', component: ServicesComponent},
-];
 
 @NgModule({
   declarations: [
@@ -34,9 +26,8 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes)
   ],
-  exports: [RouterModule],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
